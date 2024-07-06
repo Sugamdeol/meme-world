aeaf1e51c507d7c5286741381a3e83a71779f268
Sugam Deol <92591383+Sugamdeol@users.noreply.github.com>
2024-07-06 11:33:12 +0530

    Update MemeGallery.js

--- src/components/MemeGallery.js
+++ src/components/MemeGallery.js
@@ -1,15 +1,42 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import ShareButton from './ShareButton';
+import VoteButton from './VoteButton';
+import firebase from './firebase';
 
 function MemeGallery() {
   const [memes, setMemes] = useState([]);
 
   useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then((response) => response.json())
-      .then((data) => setMemes(data.data.memes))
-      .catch((error) => console.log(error));
+    const memesRef = firebase.database().ref('memes');
+    memesRef.on('value', (snapshot) => {
+      const memesData = snapshot.val();
+      const memesList = Object.entries(memesData || {}).map(([id, meme]) => ({
+        id,
+        ...meme,
+      }));
+      memesList.sort((a, b) => (b.upvotes || 0) - (b.downvotes || 0) - ((a.upvotes || 0) - (a.downvotes || 0)));
+      setMemes(memesList);
+    });
+
+    return () => {
+      memesRef.off();
+    };
   }, []);
+
+  const handleVote = (memeId, voteType) => {
+    const memeRef = firebase.database().ref(`memes/${memeId}`);
+    memeRef.transaction((meme) => {
+      if (meme) {
+        if (voteType === 'up') {
+          meme.upvotes = (meme.upvotes || 0) + 1;
+        } else if (voteType === 'down') {
+          meme.downvotes = (meme.downvotes || 0) + 1;
+        }
+      }
+      return meme;
+    });
+  };
 
   return (
     <Container>
@@ -21,6 +48,16 @@ function MemeGallery() {
               <Card.Img variant="top" src={meme.url} />
               <Card.Body>
                 <Card.Title>{meme.name}</Card.Title>
+                <ShareButton platform="twitter" url={meme.url} text={meme.name} />
+                <ShareButton platform="facebook" url={meme.url} />
+                <ShareButton platform="instagram" url={meme.url} />
+                <div>
+                  <VoteButton memeId={meme.id} voteType="up" onVote={handleVote} />
+                  <VoteButton memeId={meme.id} voteType="down" onVote={handleVote} />
+                </div>
+                <div>
+                  Upvotes: {meme.upvotes || 0} | Downvotes: {meme.downvotes || 0}
+                </div>
               </Card.Body>
             </Card>
           </Col>
